2af1d875d6ea72d18e82b1ef7cf08ade5e82aa95
kratadata <hey@zybinska.com>
2024-11-28 17:36:41 +0100

    init

--- config-local.js
+++
@@ -1,4 +0,0 @@
-export const secret_token = "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";
-export const keyword = "BRUNN" // "BRUNN" or "STUUEHL" or "BOODLE"
-export const img_size = 1024 // change to 1024 for better quality
-export const num_steps = 2 // change to 4 for better quality
